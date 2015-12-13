5c3962d710df12ae69653a1abc69ccc85265eae5
freddieiboy <freddieiboy@gmail.com>
2015-12-12 21:42:11 -0800

    refactor to searchVideo

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ var cheerio = require('cheerio');
 var URL = require('url-parse');
 var fs = require('fs');
 
-var startingUrl = "http://www.nwanime.com/"
+var startingUrl = "http://www.nwanime.com"
 var searchQuery = "One Piece"
 
-request (startingUrl, function(error, response, body) {
-  if (!error && response.statusCode == 200) {
+request(startingUrl, function(error, response, body) {
+  if (!error && response.statusCode == 200)
     // Parse the document body
     var $ = cheerio.load(body);
-    var videoLink = $('a[href^="http"]:contains(' + searchQuery + ')').attr("href");
-    var videoTitle = $('a[href^="http"]:contains(' + searchQuery + ')').attr("title");
-    //TODO: clean up title
-
-    console.log("This link is: " + videoTitle + videoLink);
+    searchVideo($);
   }
-
 });
+
+function searchVideo($) {
+  var videoLink = $('a[href^="http"]:contains(' + searchQuery + ')').attr("href");
+  var videoTitle = $('a[href^="http"]:contains(' + searchQuery + ')').attr("title");
+  console.log(videoTitle +  videoLink);
+  //TODO: clean up title
+}
