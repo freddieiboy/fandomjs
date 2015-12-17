a67df800f993b47268a9e43d13d6bee8495d92c8
freddieiboy <freddieiboy@gmail.com>
2015-12-16 21:38:33 -0800

    Add array

--- src/index.js
+++ src/index.js
@@ -4,19 +4,34 @@ var URL = require('url-parse');
 var fs = require('fs');
 
 var startingUrl = "http://www.nwanime.com"
-var searchQuery = "One Piece"
+var searchQuery = "Fairy Tail"
 
 request(startingUrl, function(error, response, body) {
   if (!error && response.statusCode == 200)
     // Parse the document body
     var $ = cheerio.load(body);
-    searchVideo($);
-  }
+    var videoLink = $('a[href^="http"]:contains(' + searchQuery + ')').attr("href");
+    var videoTitle = $('a[href^="http"]:contains(' + searchQuery + ')').attr("title");
+
+    videoCollection($, videoTitle, videoLink);
 });
 
-function searchVideo($) {
-  var videoLink = $('a[href^="http"]:contains(' + searchQuery + ')').attr("href");
-  var videoTitle = $('a[href^="http"]:contains(' + searchQuery + ')').attr("title");
-  console.log(videoTitle +  videoLink);
-  //TODO: clean up title
+// function searchVideo($) {
+//   console.log(videoTitle +  videoLink);
+//   //TODO: clean up title
+//   // fs.appendFile('videos-to-watch.txt', videoTitle + '\n' + videoLink, function(err) {
+//   //   if (err) throw err;
+//   //   console.log('Your anime\'s have been added!');
+//   // });
+// };
+
+function videoCollection($, videoTitle, videoLink) {
+  var animeToWatch = [];
+
+  animeToWatch.push({
+    title: videoTitle,
+    link: videoLink
+  });
+
+  console.log(animeToWatch);
 }
